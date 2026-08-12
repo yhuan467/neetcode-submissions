d9e36e91ba48622dcbed6fa8d442493c289ea87e
Yun <166069934+yhuan467@users.noreply.github.com>
2026-08-12 14:02:48 -0700

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+
+        HashMap<Integer,Integer> numsH = new HashMap<> ();
+        for(int i=0; i<nums.length; i++){
+            numsH.put(i, nums[i]);
+        }
+
+        //用減法去找
+        int pairValue = 0;
+        for(int i=0; i<nums.length; i++){
+            pairValue = target - nums[i];
+            if(numsH.contains(pairValue)){
+                return([numsH(pairValue),numsH(nums[i])]);
+            }
+        }
+        return[0,0];
+    }
+}
