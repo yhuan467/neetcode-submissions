39ddd4a08de0f21667befd55a0f2260593138bba
Yun <166069934+yhuan467@users.noreply.github.com>
2026-08-12 13:45:22 -0700

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        //for loop 比較頭尾
+        //注意不要管大小寫
+        for(int i=0;i<s.length(); i++){
+            if(s.CharAt[i] != s.CharAt[n-i]){
+                return False;
+            }
+        }
+        return True;
+    }
+}
