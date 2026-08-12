5ba33f94ba8b7ab76b0dbf6b14db650640bec569
Yun <166069934+yhuan467@users.noreply.github.com>
2026-08-12 13:33:19 -0700

    Add: missing-number - submission-3

---
+++ Data Structures & Algorithms/missing-number/submission-3.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        //如果長度不對 那就是幾個missing? 
+        int n = nums.length;
+        for(int i=0; i<=n; i++){
+            if(nums[i] != i){
+                return i;
+            }
+        }
+        return null;
+    }
+}
