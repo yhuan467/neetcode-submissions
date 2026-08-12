8b66a4c84172562abf5abca0b9fbc4ddaebabdb5
Yun <166069934+yhuan467@users.noreply.github.com>
2026-08-12 13:37:10 -0700

    Add: missing-number - submission-6

---
+++ Data Structures & Algorithms/missing-number/submission-6.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        //如果長度不對 那就是幾個missing? 
+        int n = nums.length;
+        Arrays.sort(nums);
+        for(int i=0; i<n; i++){
+            if(nums[i] != i){
+                return i;
+            }
+        }
+        return n;
+    }
+}
