cfd53c1a62c089338690d6177f0aa686aa8f6e5e
Yun <166069934+yhuan467@users.noreply.github.com>
2026-08-12 13:21:25 -0700

    Add: reverse-bits - submission-7

---
+++ Data Structures & Algorithms/reverse-bits/submission-7.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int reverseBits(int n) {
+        int RNum = 0;
+        for(int i=0; i<32; i++){
+            if((n&1)==1){
+                RNum = RNum | 1; //or1
+            }
+            RNum = RNum << 1; //shift left 0
+            n = n >> 1;
+        }
+        return RNum;
+    }
+}
