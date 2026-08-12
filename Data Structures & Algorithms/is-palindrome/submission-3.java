00413c4445e575bff695fb29654556e67188c1ff
Yun <166069934+yhuan467@users.noreply.github.com>
2026-08-12 13:47:21 -0700

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        //for loop 比較頭尾
+        //注意不要管大小寫
+        int n = s.length();
+        for(int i=0;i<n; i++){
+            if(s.charAt(i) != s.charAt(n-i)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
