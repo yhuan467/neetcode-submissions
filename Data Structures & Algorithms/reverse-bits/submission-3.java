087cb488d0ac8da8290cd1894625476e43028e30
Yun <166069934+yhuan467@users.noreply.github.com>
2026-08-12 13:09:27 -0700

    Add: reverse-bits - submission-3

---
+++ Data Structures & Algorithms/reverse-bits/submission-3.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int reverseBits(int n) {
+        int RNum = null;
+        if(n != 0){
+            if((n&1)==1){
+                RNum = RNum << 1; //carry 1
+            }
+            RNum = RNum << 1; //shift left 0
+            n = n >> 1;
+        }
+        return RNum;
+    }
+}
