9818a2c14697268ade677c11c33c1f2561968e91
Yun <166069934+yhuan467@users.noreply.github.com>
2026-08-13 11:18:22 -0700

    Add: is-palindrome - submission-9

---
+++ Data Structures & Algorithms/is-palindrome/submission-9.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        //for loop 比較頭尾
+        //注意大小寫 還要考慮到空格跟其他符號（問號可以省略）
+        int n = s.length();
+        if(s.charAt(n-1) == '?'){
+            n = n-1;
+        }
+        for(int i=0;i<n; i++){
+            if(s.charAt(i) != s.charAt(n-1-i)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
