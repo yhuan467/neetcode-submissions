966ec9813c714c9c2deb9bd8e713ab47e7bed165
Yun <166069934+yhuan467@users.noreply.github.com>
2026-08-13 11:38:06 -0700

    Add: is-palindrome - submission-12

---
+++ Data Structures & Algorithms/is-palindrome/submission-12.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        //for loop 比較頭尾
+        //注意大小寫
+        // 題目說了：ignores all non-alphanumeric characters..
+        int n = s.length();
+        
+        // 方案A:regex一行解決
+        s = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        //update length
+        n = s.length();
+
+        for(int i=0;i<n; i++){
+            if(s.charAt(i) != s.charAt(n-1-i)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
