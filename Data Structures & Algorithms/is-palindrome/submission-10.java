153c0d92fe8f70ec2e2a5d71388ba03a77cfa647
Yun <166069934+yhuan467@users.noreply.github.com>
2026-08-13 11:31:35 -0700

    Add: is-palindrome - submission-10

---
+++ Data Structures & Algorithms/is-palindrome/submission-10.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        //for loop 比較頭尾
+        //注意大小寫
+        // 題目說了：ignores all non-alphanumeric characters..
+        int n = s.length();
+        
+        for(int i=0;i<n; i++){
+            //移除所有空格跟標點符號
+            if(s.charAt(i) == ' ' | '?'){
+                s.remove.charAt(i);
+            }
+        }
+
+        //to lowercase
+        s = s.toLowerCase;
+        n = s.length();
+
+        for(int i=0;i<n; i++){
+            if(s.charAt(i) != s.charAt(n-1-i)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
