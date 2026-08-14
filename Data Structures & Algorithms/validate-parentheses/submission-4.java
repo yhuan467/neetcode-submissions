ee01548e9e87d063587cc0c47b531ffbb3720c65
Yun <166069934+yhuan467@users.noreply.github.com>
2026-08-14 11:21:42 -0700

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isValid(String s) {
+        //配對的一定要是偶數
+        if((s.length()%2)!=0){
+            return false;
+        }
+        //檢查有沒有配對起來
+        //從一半開始
+        int half = s.length()/2;
+        for(int i=half; i<s.length(); i++){
+            if(s.CharAt(i) == ')'){
+                if(s.CharAt(i-1)!= '('){
+                    return false;
+                }
+            }
+            else if(s.CharAt(i) == '}'){
+                if(s.CharAt(i-1)!= '{'){
+                    return false;
+                }
+            }
+            else if(s.CharAt(i) == ']'){
+                if(s.CharAt(i-1)!= '['){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
